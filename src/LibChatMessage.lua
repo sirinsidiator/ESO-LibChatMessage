8dd87604a349f8b630db7038416ee10c12c9a12a
sirinsidiator <insidiator@cmos.at>
2025-04-30 17:53:12 +0200

    fixed error in console flow

--- src/LibChatMessage.lua
+++ src/LibChatMessage.lua
@@ -651,5 +651,7 @@ EVENT_MANAGER:RegisterForEvent(LIB_IDENTIFIER, EVENT_ADD_ON_LOADED, function(eve
     end
 	LINK_HANDLER:RegisterCallback(LINK_HANDLER.LINK_CLICKED_EVENT, OnLinkClicked)
 	LINK_HANDLER:RegisterCallback(LINK_HANDLER.LINK_MOUSE_UP_EVENT, OnLinkClicked)
-	KEYBOARD_CHAT_SYSTEM:GetEditControl():SetAllowMarkupType(ALLOW_MARKUP_TYPE_ALL)
+    if not IsConsoleUI() then
+    	KEYBOARD_CHAT_SYSTEM:GetEditControl():SetAllowMarkupType(ALLOW_MARKUP_TYPE_ALL)
+    end
 end)
